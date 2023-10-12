ad357edd28ac0b9c5275510666011643028c0516
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-10-12 14:02:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const namearr = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    const thankYouCards = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return thankYouCards;
+}
+writeCards();
+
+function countDown(number){
+while (number > -1){
+  console.log (number--);
+}
+}
+countDown(10);
